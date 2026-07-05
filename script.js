78784efcb90caff3543be68ad188fa6915ced77e
thefooddude2023 <thefooddude2023@gmail.com>
2026-07-05 11:37:39 +0530

    Remove smooth scrolling for anchor links
    Removed smooth scrolling functionality for anchor links.

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
-  });
-});
+console.log("The Food Dude Website Loaded");
